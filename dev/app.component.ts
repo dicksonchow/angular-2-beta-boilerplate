81db2028eae05b3d46a91206572a653807477f3a
Dickson Chow <dicksonchow2007@gmail.com>
2017-05-11 00:00:42 +0800

    Finish the first contact component

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,10 +3,30 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <h1 
+          (click)="onSelect()"
+          [class.clicked]="showDetail === true"
+        >{{contact.firstName}} {{contact.lastName}}</h1>
+        <input [(ngModel)] = "contact.firstName" type="text"/>
+        <div *ngIf="showDetail === true">
+          Phone Number: {{contact.phone}} <br>
+          E-Mail: {{contact.email}}
+        </div>
     `,
+    styleUrls: ["../src/css/app.css"]
 })
 export class AppComponent {
 
+    public contact = {
+      firstName: "Dickson",
+      lastName: "Chow",
+      phone: "12345678",
+      email: "1234@gmail.com"
+    };
+
+    public showDetail = false;
+
+    onSelect() {
+      this.showDetail = true;
+    };
 }
